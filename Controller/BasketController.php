a203749c2462d69044f24b856661f4dcd29bf999
robin <robin.19thomas@gmail.com>
2023-06-27 16:53:23 +0200

    initialisation panier

--- Controller/BasketController.php
+++ Controller/BasketController.php
@@ -2,10 +2,17 @@
 // Contrôleur Panier
 namespace Controller;
 
-class BasketController {
+class BasketController extends BaseController{
     
+    function ShowCart(){
+        $products = $_SESSION['panier'];
+        $this->compact(["products" => $products]);
+        $total = $this -> calculatePrice();
+        $this->compact(["total" => $total]);
+        $this->view("Basket");
+    }
 
-    public function AddToCart($article, $name, $quantite, $price) {
+    public function AddToCart($article, $name, $quantite, $image, $description, $price) {
         if (!isset($_SESSION['panier'])) {
             $_SESSION['panier'] = array();
         }
@@ -15,27 +22,32 @@ class BasketController {
             $_SESSION['panier'][$article]['quantite'] = $quantite;
             $_SESSION['panier'][$article]["prix"] = $_SESSION['panier'][$article]['quantite']*$price;
         }else{
+            $_SESSION['panier'][$article]['id'] = $article;
             $_SESSION['panier'][$article]['name'] = $name;
             $_SESSION['panier'][$article]['quantite'] = $quantite;
             $_SESSION['panier'][$article]["prix"] = $quantite * $price;
+            $_SESSION['panier'][$article]["description"] = $description;
+            $_SESSION['panier'][$article]["image"] = $image;
         }
     }
 
-    public function DeleteFromCart($article) {
+    public function DeleteFromCart($id) {
         if (isset($_SESSION['panier'])) {
-            unset($_SESSION['panier'][$article]);
+            unset($_SESSION['panier'][$id]);
         }
+        header('Location: /basket');
     }
 
-    public function UpdateQuantity($article, $quantite) {
-        if(isset($_SESSION['panier'][$article])){
-            $_SESSION['panier'][$article]['quantite']= $quantite;
+    public function UpdateQuantity($id , $quantite) {
+        if(isset($_SESSION['panier'][$id])){
+            $_SESSION['panier'][$id]['quantite']= $quantite;
         }
+
+        header('Location: /basket');
     }
 
     public function calculatePrice() {
         $total = 0;
-
         
         if (isset($_SESSION['panier'])) {
             
@@ -43,13 +55,41 @@ class BasketController {
                     
                $total += $PannierElement['prix'];
             }
+            return $total;
         }
-
-        return $total;   
+        return $total;
     }
 }
 
 
 
- $panierController = new BasketController();
+ $panierController = new BasketController("basket");
+
+ $panierController->AddToCart(  12, 
+                                "Carte Graphique RTX 3060", 
+                                1, 
+                                "https://placehold.co/250x200?font=roboto", 
+                                "GeForce RTX 3060
+                                Type de bus : PCI-Express 4.0 x16
+                                Horloge boostée : 1777 MHz
+                                Interfaces : DisplayPort 1.4a (x3), HDMI 2.1
+                                Technologie : GDDR6 
+                                API prises en charge : Vulkan RT API, OpenGL 4.6 ", 
+                                "350.99");
+
+$panierController->AddToCart(   15, 
+                                "Carte Graphique RTX 3060", 
+                                1, 
+                                "https://placehold.co/250x200?font=roboto   ", 
+                                "GeForce RTX 3060
+                                Type de bus : PCI-Express 4.0 x16
+                                Horloge boostée : 1777 MHz
+                                Interfaces : DisplayPort 1.4a (x3), HDMI 2.1
+                                Technologie : GDDR6 
+                                API prises en charge : Vulkan RT API, OpenGL 4.6 ", 
+                                "350.99"
+);
+
+
+
 
