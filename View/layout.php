5fb9483723494455e89c170d2165f8f86457ffad
jderache <contact@julienderache.fr>
2023-06-28 00:34:16 +0200

    Menu burger on mobile

--- View/layout.php
+++ View/layout.php
@@ -23,29 +23,44 @@
             <a aria-label="HomePage" href="/" title="SellMeOut" class="logoLink">
                 <img alt="SellMeOut" src="../Images/logo.png" class="logo">
             </a>
-                <div class="navigation">
-                    <a class="<?php if ($first_part=="products") {echo "active"; } else  {echo "noactive";}?>" href="/products">Les produits en vente</a>
-                    <a class="<?php if ($first_part=="basket") {echo "active"; } else  {echo "noactive";}?>" href="/basket">Panier&nbsp;<i class="fa-solid fa-basket-shopping"></i></a>
-                    <?php if(!isset($_SESSION['user'])){ ?>
-                        <a class="<?php if ($first_part=="login") {echo "active"; } else  {echo "noactive";}?>" href="/login">Login&nbsp;<i class="fa-solid fa-right-to-bracket"></i></a>
-                    <?php } ?>
-                    <?php if(isset($_SESSION['user'])){ ?>
-                        <a class="<?php if ($first_part=="logout") {echo "active"; } else  {echo "noactive";}?>" href="/user"><i class="fa-solid fa-user"></i></a>
-                    <?php } ?>
-                    <?php if(isset($_SESSION['user']) && $_SESSION['user']->role == "seller"){ ?>
-                        <a class="add-product my-products <?php if ($first_part=="user") {echo "active"; } else  {echo "noactive";}?>" href="/my-products">Mes produits</a>
-                    <?php } ?>
-                    <?php if(isset($_SESSION['user']) && $_SESSION['user']->role == "seller"){ ?>
-                        <a class="add-product <?php if ($first_part=="product") {echo "active"; } else  {echo "noactive";}?>" href="/product/new">Ajouter un produit</a>
-                    <?php } ?>
-                    <?php if(isset($_SESSION['user'])){ ?>
-                        <a class="logout-btn <?php if ($first_part=="logout") {echo "active"; } else  {echo "noactive";}?>" href="/logout">Logout&nbsp;<i class="fa-solid fa-right-to-bracket"></i></a>
-                    <?php } ?>
-                </div>
+            <div class="navigation">
+                <a class="<?php if ($first_part=="products") {echo "active"; } else  {echo "noactive";}?>" href="/products">Les produits en vente</a>
+                <a class="<?php if ($first_part=="basket") {echo "active"; } else  {echo "noactive";}?>" href="/basket">Panier&nbsp;<i class="fa-solid fa-basket-shopping"></i></a>
+                <?php if(!isset($_SESSION['user'])){ ?>
+                    <a class="<?php if ($first_part=="login") {echo "active"; } else  {echo "noactive";}?>" href="/login">Login&nbsp;<i class="fa-solid fa-right-to-bracket"></i></a>
+                <?php } ?>
+                <?php if(isset($_SESSION['user'])){ ?>
+                    <a class="<?php if ($first_part=="logout") {echo "active"; } else  {echo "noactive";}?>" href="/user"><i class="fa-solid fa-user"></i></a>
+                <?php } ?>
+                <?php if(isset($_SESSION['user']) && $_SESSION['user']->role == "seller"){ ?>
+                    <a class="add-product my-products <?php if ($first_part=="user") {echo "active"; } else  {echo "noactive";}?>" href="/my-products">Mes produits</a>
+                <?php } ?>
+                <?php if(isset($_SESSION['user']) && $_SESSION['user']->role == "seller"){ ?>
+                    <a class="add-product <?php if ($first_part=="product") {echo "active"; } else  {echo "noactive";}?>" href="/product/new">Ajouter un produit</a>
+                <?php } ?>
+                <?php if(isset($_SESSION['user'])){ ?>
+                    <a class="logout-btn <?php if ($first_part=="logout") {echo "active"; } else  {echo "noactive";}?>" href="/logout">Logout&nbsp;<i class="fa-solid fa-right-to-bracket"></i></a>
+                <?php } ?>
+            </div>
+            <div id="nav-icon1">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </div>
     </header>
     <div class="content">
         <?= $content; ?>
     </div>
+
+    <script>
+        const navIcons = document.querySelectorAll("#nav-icon1");
+        navIcons.forEach(function(navIcon) {
+            navIcon.addEventListener('click', function() {
+                this.classList.toggle('open');
+                document.querySelector('.navigation').classList.toggle('open');
+            });
+        });
+    </script>
     
 
