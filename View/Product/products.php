9af36d36549649ce9b8a35bdb5f25c4f74c44e71
Ribeiro Thomas <tribeiro@auchan.fr>
2023-06-28 11:47:48 +0200

    Recherche d'un produit sur la page produit

--- View/Product/products.php
+++ View/Product/products.php
@@ -1,18 +1,22 @@
 <div class="container">
     <div class="products">
         <h2>Liste des produits en vente</h2>
+        <input type="search" id="search">
+        <a href="/products/search/" id="search-btn">Rechercher</a>
         <div class="products-list">
             <?php if(empty($products)): ?>
-                <p style="text-align: center; color:#F81649;">Il n'y a pas de produit en vente pour le moment.<br>Revenez plus tard !</p>
+                <p style="text-align: center; color:#F81649;">Il n'y a pas de produit en vente pour cette recherche moment.<br>Revenez plus tard !</p>
             <?php endif; ?>
 
             <?php foreach($products as $product): ?>
-                <div class="product-card" data-href="/Product/<?= $product->id ?>">
+                <div class="product-card" data-href="/product/<?= $product->id ?>">
                     <img src="<?= $product->image ?>" alt="">
                     <h3><?= $product->nom ?></h3>
                     <p><?= $product->description ?></p>
                     <p><?= $product->price ?> €</p>
-                    <p><?= $product->pseudo ?></p>
+                    <?php if(isset($product->pseudo)): ?>
+                        <p><?= $product->pseudo ?></p>
+                    <?php endif; ?>
                     <form method="post" action="/basket/add">
                         <input type="hidden" name="id" value="<?= $product->id ?>">
                         <input type="submit" value="Ajouter au panier">
@@ -20,6 +24,11 @@
                 </div>
             <?php endforeach; ?>
 
+            <script>
+                document.getElementById("search").addEventListener("keyup", function(event) {
+                    document.getElementById("search-btn").href = encodeURI("/products/search/" + event.target.value);
+                });
+            </script>
         </div>
     </div>
 </div>
