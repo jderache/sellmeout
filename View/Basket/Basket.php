00d3cb52bdbdb6829ed8a067713dcc5d2589a435
Ribeiro Thomas <tribeiro@auchan.fr>
2023-06-28 12:45:58 +0200

    changement style sur basket

--- View/Basket/Basket.php
+++ View/Basket/Basket.php
@@ -18,25 +18,25 @@
                             <input type="hidden" name="id" value="<?=$product['id']?>">
                             <label for="quantite">Quantité : </label>
                             <input class='IptQte' id='quantite' name="quantite" type="number" min="0" step="1" value="<?= $product['quantite'];?>">
-                            <input type="submit">
+                            <input type="submit" value="Mettre à jour le panier" class="reload-cart">
                         </form>
                     </div>
                     <p class='description'><?=$product['description']?></p>
                 </div>
             </div>     
- <?php  endforeach;?>
-        <h3>Prix total : <?=$total?>€</h3>
-        <form method="POST" action="/basket/confirm">
-          <button class="Commander">
-            <i class="fa-solid fa-credit-card"></i>
-            Commander
-          </button>
-        </form>
-    <?php } else{ ?>
-        <div class="EmptyCart">
-            <span>Aucun article dans votre panier.</span>
-        </div>
-<?php
-    }
-?>
+        <?php  endforeach;?>
+                <h3>Prix total : <?=$total?>€</h3>
+                <form method="POST" action="/basket/confirm">
+                <button class="Commander">
+                    <i class="fa-solid fa-credit-card"></i>
+                    Commander
+                </button>
+                </form>
+            <?php } else{ ?>
+                <div class="EmptyCart">
+                    <span>Aucun article dans votre panier.</span>
+                </div>
+            <?php
+                }
+            ?>
 </div>
