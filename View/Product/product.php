da7c6325e4db37272176f1dcd876dfe2864e32f9
jderache <contact@julienderache.fr>
2023-06-29 16:32:13 +0200

    Single product page

--- View/Product/product.php
+++ View/Product/product.php
@@ -2,10 +2,33 @@
   <h1>Produit <?= $product->nom ?></h1>
   <div class="product">
     <div class="product-card">
-      <img src="<?= $product->image ?>" alt="">
-      <h3><?= $product->nom ?></h3>
-      <p><?= $product->description ?></p>
-      <p><?= $product->price ?> €</p>
-      <p><?= $product->pseudo ?></p>
+      <div class="product-image">
+        <img src="<?= $product->image ?>" alt="">
+      </div>
+      <div>
+        <h3><?= $product->nom ?></h3>
+        <p><?= $product->description ?></p>
+        <p><?= $product->price ?> €</p>
+        <p><?= $product->pseudo ?></p>
+        <?php if(isset($_SESSION['user']) && $_SESSION['user']->role == "seller"): ?>
+        <?php else: ?>
+            <form method="post" action="/basket/add">
+                <input type="hidden" name="id" value="<?= $product->id ?>">
+                <div class="button-add">
+                    <button type="submit" class="button-add-to-cart" value="Ajouter au panier"> 
+                        Ajouter au panier
+                        <i class="fa-sharp fa-solid fa-cart-plus"></i>
+                    </button>
+                </div>
+            </form>
+        <?php endif; ?>
+        <div class="stars">
+          <?php 
+          for ($i = 0; $i < $product->rate; $i++) {
+              echo '<i class="fa-solid fa-star" id="stars"></i>';
+          }
+          ?>
+        </div>
+      </div>
     </div>
 </div>
--- View/Product/products.php
+++ View/Product/products.php
@@ -16,34 +16,34 @@
 
             <?php foreach($products as $product): ?>
                 <a href="/product/<?= $product->id ?>">
-                <div class="product-card" data-href="/product/<?= $product->id ?>">
-                    <img src="<?= $product->image ?>" alt="">
-                    <h3><?= $product->nom ?></h3>
-                    <p><?= $product->description ?></p>
-                    <p><?= $product->price ?> €</p>
-                    <p>Notes :</p>
-                    <div class="stars">
-                        <?php 
-                        for ($i = 0; $i < $product->rate; $i++) {
-                            echo '<i class="fa-solid fa-star" id="stars"></i>';
-                        }
-                        ?>
-                    </div>
-                    <p><?= $product->pseudo ?></p>
+                    <div class="product-card" data-href="/product/<?= $product->id ?>">
+                        <img src="<?= $product->image ?>" alt="">
+                        <h3><?= $product->nom ?></h3>
+                        <p><?= $product->description ?></p>
+                        <p><?= $product->price ?> €</p>
+                        <p>Notes :</p>
+                        <div class="stars">
+                            <?php 
+                            for ($i = 0; $i < $product->rate; $i++) {
+                                echo '<i class="fa-solid fa-star" id="stars"></i>';
+                            }
+                            ?>
+                        </div>
+                        <p><?= $product->pseudo ?></p>
 
-                    <?php if(isset($_SESSION['user']) && $_SESSION['user']->role == "seller"): ?>
-                    <?php else: ?>
-                        <form method="post" action="/basket/add">
-                            <input type="hidden" name="id" value="<?= $product->id ?>">
-                            <div class="button-add">
-                                <button type="submit" class="button-add-to-cart" value="Ajouter au panier"> 
-                                    Ajouter au panier
-                                    <i class="fa-sharp fa-solid fa-cart-plus"></i>
-                                </button>
-                            </div>
-                        </form>
-                    <?php endif; ?>
-                </div>
+                        <?php if(isset($_SESSION['user']) && $_SESSION['user']->role == "seller"): ?>
+                        <?php else: ?>
+                            <form method="post" action="/basket/add">
+                                <input type="hidden" name="id" value="<?= $product->id ?>">
+                                <div class="button-add">
+                                    <button type="submit" class="button-add-to-cart" value="Ajouter au panier"> 
+                                        Ajouter au panier
+                                        <i class="fa-sharp fa-solid fa-cart-plus"></i>
+                                    </button>
+                                </div>
+                            </form>
+                        <?php endif; ?>
+                    </div>
                 </a>
             <?php endforeach; ?>
 
